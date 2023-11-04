3f0e32160fab5fd5066d7b4422f00e835bb1aadc
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-04 20:06:06 +0530

    Modify header & navbar to handle loading & auth

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRightIcon } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react";
+
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -19,10 +26,27 @@ export const Heading = () => {
         <br />
         better, faster work happens. Now with AI✨
       </h3>
-      <Button>
-        Enter Notion
-        <ArrowRightIcon className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size={"lg"} />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Notion
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Notion free
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { userScrollTop } from "@/hooks/use-scroll-top";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
 import { cn } from "@/lib/utils";
-import { Logo } from "./logo";
+import { userScrollTop } from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 export const Navbar = () => {
+  // convex authentication
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  // user scroll top
   const scrolled = userScrollTop();
 
   return (
@@ -17,6 +27,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={"sm"}>Get Notion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"}>
+              <Link href="/documents">Enter Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
