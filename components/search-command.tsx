bfbcf7469ca437d58aeafaaa07f342f5e63f9e6f
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-10 21:26:39 +0530

    tsx part for search component

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -33,9 +33,51 @@ export const SearchCommand = () => {
     setIsMounted(true);
   }, []);
 
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        toggle();
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, [toggle]);
+
   if (!isMounted) {
     return null;
   }
 
-  return;
+  // on select document function
+  const onSelect = (id: string) => {
+    router.push(`/documents/${id}`);
+    onClose();
+  };
+
+  return (
+    <CommandDialog open={isOpen} onOpenChange={onClose}>
+      <CommandInput placeholder={`Search ${user?.fullName}'s Notion...`} />
+      <CommandList>
+        <CommandEmpty>No results found</CommandEmpty>
+        <CommandGroup heading={"Documents"}>
+          {documents?.map((document) => (
+            <CommandItem
+              key={document._id}
+              value={`${document._id}-${document.title}`}
+              title={document.title}
+              onSelect={onSelect}
+            >
+              {document.icon ? (
+                <p className="mr-2 text-[18px]">{document.icon}</p>
+              ) : (
+                <File className="mr-2 h-4 w-4" />
+              )}
+              <span>{document.title}</span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
 };
