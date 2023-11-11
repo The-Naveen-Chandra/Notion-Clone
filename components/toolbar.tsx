12aa3b9752533fe6287de9adeded2a4cbf665f5e
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-12 00:24:34 +0530

    Refactor toolbar component to enable editing of document title

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { Doc } from "@/convex/_generated/dataModel";
+import React, { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { ImageIcon, Smile, X, icons } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
 
 import { IconPicker } from "./icon-picker";
-import { ImageIcon, Smile, X, icons } from "lucide-react";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -12,6 +16,44 @@ interface ToolbarProps {
 }
 
 export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (preview) {
+      return;
+    }
+
+    setIsEditing(true);
+
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
