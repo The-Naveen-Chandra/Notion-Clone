53cf874bbd7d384ce6e7699c6bcfdb060dba5229
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-13 23:47:41 +0530

    Added remotePatterns for images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // images: {
+  //   domains: ["files.edgestore.dev"],
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
