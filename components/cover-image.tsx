c9f38f6750ee439c83ba82703126a3607cb73899
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-13 23:47:29 +0530

    Created CoverImage component with change and remove cover buttons

---
+++ components/cover-image.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+
+interface CoverImageProps {
+  url?: string;
+  preview?: boolean;
+}
+
+export const CoverImage = ({ url, preview }: CoverImageProps) => {
+  const coverImage = useCoverImage();
+
+  return (
+    <div
+      className={cn(
+        "relative w-full h-[35vh] group",
+        !url && "h-[12vh]",
+        url && "bg-muted"
+      )}
+    >
+      {!!url && (
+        <Image src={url} fill alt="Cover Image" className="object-cover" />
+      )}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-80 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className=" text-muted-foreground text-xs"
+            variant={"outline"}
+            size={"sm"}
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change cover
+          </Button>
+          <Button
+            onClick={() => {}}
+            className=" text-muted-foreground text-xs"
+            variant={"outline"}
+            size={"sm"}
+          >
+            <X className="h-4 w-4 mr-2" />
+            Remove
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
